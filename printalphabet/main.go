cf42e303a540f88d9d0b80f396956060d2d157e2
antaww <anta.pizzetta@gmail.com>
2021-09-13 21:44:31 +0200

    6th commit

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 
 	var aRune string = "abcdefghijklmnopqrstuvwxyz'\n'" //string a lire
-	for i := 0; i < 26; i++ {                           //boucle 26 rep
+	for i := 0; i < 26; i++ {                           //boucle 26 repp
 		z01.PrintRune(rune(aRune[i])) //rune valeur i pour incrementer
 	}
 }
