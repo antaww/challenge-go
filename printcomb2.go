e5d64fe7fd5bd519a5adbe9b012ff7af1602f4ce
antaww <anta.pizzetta@gmail.com>
2021-09-14 16:10:19 +0200

    comb2

--- printcomb2.go
+++ printcomb2.go
@@ -8,7 +8,7 @@ func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
-				for l := j + '1'; l <= '9'; l++ {
+				for l := j + 1; l <= '9'; l++ {
 					if i > k || (i == k && j <= l) {
 						z01.PrintRune(i)
 						z01.PrintRune(j)
