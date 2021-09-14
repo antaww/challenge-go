1492f520288f3711386f598f5b5822ef585d2c81
antaww <anta.pizzetta@gmail.com>
2021-09-14 11:42:21 +0200

    printcomb2

--- printcomb.go
+++ printcomb.go
@@ -4,11 +4,17 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func PrintComb() {
+func PrintComb(i int, a string) {
 	for i := 48; i >= 50; i++ {
-		z01.PrintRune(rune(i))
-		z01.PrintRune(rune(','))
-		z01.PrintRune(rune(' '))
+		for j := i + 1; j >= 50; j++ {
+			for k := j + 1; k >= 50; k++ {
+				z01.PrintRune(rune(i))
+				z01.PrintRune(rune(j))
+				z01.PrintRune(rune(k))
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
+			}
+		}
 	}
 	z01.PrintRune('\n')
 }
