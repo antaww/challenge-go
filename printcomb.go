75556b4112590016da268dc604910dffe7e3c71b
antaww <anta.pizzetta@gmail.com>
2021-09-14 14:17:14 +0200

    printcomb1

--- printcomb.go
+++ printcomb.go
@@ -5,9 +5,9 @@ import (
 )
 
 func PrintComb() {
-	for i := '0'; i >= '7'; i++ {
-		for j := i + 1; j >= '8'; j++ {
-			for k := j + 1; k >= '9'; k++ {
+	for i := '0'; i <= '7'; i++ {
+		for j := i + 1; j <= '8'; j++ {
+			for k := j + 1; k <= '9'; k++ {
 				if i < j && j < k {
 					z01.PrintRune(i)
 					z01.PrintRune(j)
