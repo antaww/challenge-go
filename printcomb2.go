c93ddfea3c71534af845a05854d09dc5943a2575
antaww <anta.pizzetta@gmail.com>
2021-09-14 15:58:46 +0200

    comb2

--- printcomb2.go
+++ printcomb2.go
@@ -5,31 +5,24 @@ import (
 )
 
 func piscine() {
-	for i := '0'; i <= '8'; i++ {
-		for j := i + '1'; j <= '8'; j++ {
-			for k := j + '1'; k <= '8'; k++ {
-				for l := k + '1'; l <= '8'; l++ {
-					if i>k || (a==c && b>=d) {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			for k := '0'; k <= '9'; k++ {
+				for l := j + '1'; l <= '9'; l++ {
+					if i > k || (i == k && j >= l) {
 						z01.PrintRune(i)
 						z01.PrintRune(j)
 						z01.PrintRune(32)
 						z01.PrintRune(k)
 						z01.PrintRune(l)
-						if k == '9' && l == '9' {
-							j := j + '1'
-							k := k - '9'
-							l := l - '9'
-							if i == '9' && j == '8' && k == '9' && l == '9' {
-								z01.PrintRune(10)
-							} else {
-								z01.PrintRune(44)
-								z01.PrintRune(32)
-							}
-
-					}
-						
+						if i == '9' && j == '8' && k == '9' && l == '9' {
+							z01.PrintRune(10)
+						} else {
+							z01.PrintRune(44)
+							z01.PrintRune(32)
 						}
 					}
+
 				}
 			}
 		}
