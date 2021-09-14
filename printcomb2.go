f510fbe9ff031eebea191bf9dde0a97a307a1847
antaww <anta.pizzetta@gmail.com>
2021-09-14 14:42:39 +0200

    printcomb2 1st

---
+++ printcomb2.go
@@ -0,0 +1,36 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func PrintComb2() {
+	for i := '0'; i <= '8'; i++ {
+		for j := i + 1; j <= '8'; j++ {
+			for k := j + 1; k <= '8'; k++ {
+				for l := k + 1; l <= '8'; l++ {
+					if k <= 9 && l <= 9 {
+						j := j + 1
+						k := k - 9
+						l = l - 9
+						if j <= 9 {
+							i := i + 1
+							z01.PrintRune(i)
+							z01.PrintRune(j)
+							z01.PrintRune(32)
+							z01.PrintRune(k)
+							z01.PrintRune(l)
+							if i == '9' && j == '9' {
+								z01.PrintRune(10)
+							} else {
+								z01.PrintRune(44)
+								z01.PrintRune(32)
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
