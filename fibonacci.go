76454323204bac4544bde68e0824e5138ec0877f
antaww <anta.pizzetta@gmail.com>
2021-09-15 14:34:04 +0200

    fibo

--- fibonacci.go
+++ fibonacci.go
@@ -1,11 +1,11 @@
 package piscine
 
 func Fibonacci(index int) int {
-	if index < 2 {
-		return index
-	}
 	if index < 0 {
 		return -1
+	}
+	if index < 2 {
+		return index
 	}
 	return Fibonacci(index-2) + Fibonacci(index-1)
 }
