d099a922293b26aaf9047be83e111e233017913d
antaww <anta.pizzetta@gmail.com>
2021-09-15 12:07:58 +0200

    power

--- iterativepower.go
+++ iterativepower.go
@@ -9,7 +9,7 @@ func IterativePower(nb int, power int) int {
 	if power < 0 {
 		return 0
 	} else {
-		for i := 1; i <= power; i++ {
+		for i := 1; i <= power-1; i++ {
 			result = nb * result
 		}
 	}
