5d5b4873dd4755c95fee7cfd6d38a21dd090993f
antaww <anta.pizzetta@gmail.com>
2021-09-15 09:56:01 +0200

    iterative

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -5,14 +5,14 @@ func IterativeFactorial(index int) int {
 
 	if index <= 0 || index > 9223372036854775807 {
 		return 0
+	}
+	if index > 20 {
+		return 0
 	} else {
 		for i := 1; i <= index; i++ {
 			result = result * i
 		}
 		if result <= 0 || result < 9223372036854775807 {
-			return 0
-		}
-		if index > 20 {
 			return 0
 		}
 	}
