788f74e9c55c7a394c20a2b48cba3284801d6740
antaww <anta.pizzetta@gmail.com>
2021-09-15 11:34:36 +0200

    recursive

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -11,9 +11,10 @@ func RecursiveFactorial(index int) int {
 	}
 	if index > 20 {
 		return 0
-	} else if index >= 1 {
-		index = index - 1
-		result = result * index
+	} else if index == 1 {
+		return 1
+	} else if index > 1 {
+		return index * RecursiveFactorial(index-1)
 	}
 	return result
 }
