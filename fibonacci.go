34f8959bfb74ea92e53a053312e1e43ac9f6f1f5
antaww <anta.pizzetta@gmail.com>
2021-09-15 14:19:39 +0200

    fibonacci

--- fibonacci.go
+++ fibonacci.go
@@ -3,8 +3,11 @@ package piscine
 func Fibonacci(index int) int {
 	if index < 0 {
 		return -1
+	}
+	if index == 0 {
+		return 0
 	} else {
 		index = 0
-		return index + Fibonacci(index+1)
+		return (index + Fibonacci(index+1))
 	}
 }
