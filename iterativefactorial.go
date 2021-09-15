82c0a2aab660c3e6e1c6c277558db864614f938d
antaww <anta.pizzetta@gmail.com>
2021-09-15 10:12:31 +0200

    iterative

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,22 +1,16 @@
 package piscine
 
 func IterativeFactorial(index int) int {
-	result := 1
-	if index == 0 {
-		index = 4
-	}
+
 	if index <= 0 || index > 9223372036854775807 {
 		return 0
 	}
 	if index > 20 {
 		return 0
 	} else {
-		for i := 1; i <= index; i++ {
-			result = result * i
-		}
-		if result <= 0 || result < 9223372036854775807 {
-			return 0
+		for i := index; i <= index; i++ {
+			index = index * i
 		}
 	}
-	return result
+	return index
 }
