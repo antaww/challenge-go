0818d11dc09af85c9966da35a2b59d39ce870c64
antaww <anta.pizzetta@gmail.com>
2021-09-15 14:30:32 +0200

    recursivepower

--- fibonacci.go
+++ fibonacci.go
@@ -1,11 +1,11 @@
 package piscine
 
 func Fibonacci(index int) int {
+	if index < 2 {
+		return index
+	}
 	if index < 0 {
 		return -1
-	}
-	if index == 0 {
-		return 0
 	} else {
 		return (Fibonacci(index-2) + Fibonacci(index-1))
 	}
