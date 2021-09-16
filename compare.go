b39b0531490a0c899af11d2577ad5237d440f855
antaww <anta.pizzetta@gmail.com>
2021-09-16 10:14:16 +0200

    compare

--- compare.go
+++ compare.go
@@ -1,19 +1,11 @@
 package piscine
 
-func FirstRune(s string) rune {
-	return []rune(s)[0]
-}
-
-func LastRune(s string) rune {
-	return []rune(s)[len(s)-1]
-}
-
 func Compare(a, b string) int {
 	if a == b {
 		return 0
-	} else if FirstRune(a) != FirstRune(b) {
+	} else if a > b {
 		return -1
-	} else if LastRune(a) != LastRune(b) {
+	} else if a < b {
 		return 1
 	}
 }
