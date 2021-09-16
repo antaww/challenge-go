e75d1a84cbee4ec1d00644007c17140c76a4d81a
antaww <anta.pizzetta@gmail.com>
2021-09-16 13:54:16 +0200

    alpha

--- alphacount.go
+++ alphacount.go
@@ -2,10 +2,8 @@ package piscine
 
 func AlphaCount(s string) int {
 	counter := 0
-	str := []rune(s)
-
-	for i := 0; i <= len(str); i++ {
-		if (str[i] >= 65 && str[i] <= 90) || (str[i] >= 97 && str[i] <= 122) {
+	for _, letter := range s {
+		if (letter >= 65 && letter <= 90) || (letter >= 97 && letter <= 122) {
 			counter++
 		}
 	}
