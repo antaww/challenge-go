fbdbce985e32f2dcf722624a38c0ba8feb71bd8b
antaww <anta.pizzetta@gmail.com>
2021-09-16 17:17:44 +0200

    toupper

---
+++ toupper.go
@@ -0,0 +1,11 @@
+package piscine
+
+func ToUpper(s string) string {
+	var phrase string
+	for i := 0; i <= len(phrase)-1; i++ {
+		if i >= 'a' && i <= 'z' {
+			phrase = string(s[i] - 32)
+		}
+	}
+	return phrase
+}
