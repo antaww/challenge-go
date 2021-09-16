c0e8b317e071580c32bea570a8d87f8341238da8
antaww <anta.pizzetta@gmail.com>
2021-09-16 16:03:02 +0200

    isalpha

--- isalpha.go
+++ isalpha.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for _, letter := range s {
-		if !((letter >= 'A' && letter <= 'Z') || (letter >= 'a' && letter <= 'z')) {
+		if !((letter >= 'A' && letter <= 'Z') || (letter >= 'a' && letter <= 'z') || (letter >= '0' && letter <= '9')) {
 			return false
 		}
 	}
