6909a6a1e023c47ad9c7d1777b43a22ffcf23515
antaww <anta.pizzetta@gmail.com>
2021-09-16 16:33:26 +0200

    isprintable

--- isprintable.go
+++ isprintable.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsPrintable(s string) bool {
 	for _, letter := range s {
-		if letter >= 0 && letter <= 32 {
+		if !(letter >= 33 && letter <= 255) {
 			return false
 		}
 	}
