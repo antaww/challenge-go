7da68e7b5d16abb12b6745a923d1bc210d9e6ca8
antaww <anta.pizzetta@gmail.com>
2021-09-16 15:25:42 +0200

    isupper

--- isupper.go
+++ isupper.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsUpper(s string) bool {
 	for _, letter := range s {
-		if letter >= 'a' && letter <= 'z' {
+		if !(letter >= 'A' && letter <= 'Z') {
 			return false
 		}
 	}
