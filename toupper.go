4175fb59ad619fa63300971bc6ea85d1015a7f96
antaww <anta.pizzetta@gmail.com>
2021-09-16 17:27:07 +0200

    toupper

--- toupper.go
+++ toupper.go
@@ -4,7 +4,7 @@ func ToUpper(s string) string {
 	var phrase string
 	for i := 0; i <= len(s)-1; i++ {
 		if i >= 'a' && i <= 'z' {
-			phrase += string(s[i] - 32)
+			phrase = string(s[i] - 32)
 		}
 		phrase += string(s[i])
 	}
