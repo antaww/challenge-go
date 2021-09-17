2a49f7aa11355fa4790eab1a37ad88430fb8c22c
antaww <anta.pizzetta@gmail.com>
2021-09-17 17:22:01 +0200

    capitalize

--- capitalize.go
+++ capitalize.go
@@ -2,15 +2,17 @@ package piscine
 
 func Capitalize(s string) string {
 	sRune := []rune(s)
-	for i := 0; i <= len(s)-1; i++ {
-		if (sRune[i] >= 'A' && sRune[i] <= 'Z') || (sRune[i] >= 'a' && sRune[i] <= 'z') || (sRune[i] >= '0' && sRune[i] <= '9') {
-			if s[i-1] == ' ' {
+	lettre := true
+	for i := 0; i < len(s); i++ {
+		if ((sRune[i] >= 'A' && sRune[i] <= 'Z') || (sRune[i] >= 'a' && sRune[i] <= 'z') || (sRune[i] >= '0' && sRune[i] <= '9')) && lettre == true {
+			if sRune[i] >= 'a' && sRune[i] <= 'z' {
 				sRune[i] += sRune[i-32]
-			} else {
-				sRune[i] += sRune[i]
+				lettre = false
+			} else if sRune[i] >= 'A' && sRune[i] <= 'Z' {
+				sRune[i] += sRune[i+32]
 			}
 		} else {
-			sRune[i] += sRune[i]
+			lettre = true
 		}
 	}
 	return string(sRune)
