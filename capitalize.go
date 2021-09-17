a4b74a840019d30943fbe42931a7242ad0a601c2
antaww <anta.pizzetta@gmail.com>
2021-09-17 17:35:30 +0200

    capitalize

--- capitalize.go
+++ capitalize.go
@@ -1,17 +1,23 @@
 package piscine
 
+func verif(a rune) bool {
+	if (a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'z') || (a >= '0' && a <= '9') {
+		return true
+	}
+	return false
+}
 func Capitalize(s string) string {
 	sRune := []rune(s)
 	lettre := true
 	for i := 0; i < len(s); i++ {
-		if ((sRune[i] >= 'A' && sRune[i] <= 'Z') || (sRune[i] >= 'a' && sRune[i] <= 'z') || (sRune[i] >= '0' && sRune[i] <= '9')) && lettre == true {
+		if verif(sRune[i]) == true && lettre {
 			if sRune[i] >= 'a' && sRune[i] <= 'z' {
-				sRune[i] += sRune[i-32]
-				lettre = false
-			} else if sRune[i] >= 'A' && sRune[i] <= 'Z' {
-				sRune[i] += sRune[i+32]
+				sRune[i] += 'A' - 'a'
 			}
-		} else {
+			lettre = false
+		} else if sRune[i] >= 'A' && sRune[i] <= 'Z' {
+			sRune[i] += 'a' - 'A'
+		} else if verif(sRune[i]) == false {
 			lettre = true
 		}
 	}
