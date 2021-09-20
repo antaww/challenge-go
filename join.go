b478429f16accdec413266896793b7d3a34e77ca
antaww <anta.pizzetta@gmail.com>
2021-09-20 08:45:13 +0200

    join

--- join.go
+++ join.go
@@ -3,10 +3,9 @@ package piscine
 func Join(strs []string, sep string) string {
 	var phrase string
 	for i := 0; i < len(strs); i++ {
+		phrase = phrase + strs[i]
 		if i < len(strs)-1 {
 			phrase = phrase + sep
-		} else {
-			phrase = phrase + strs[i]
 		}
 	}
 	return phrase
