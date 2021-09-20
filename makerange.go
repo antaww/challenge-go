ff8ac8ec9c2390a3011e016043c09a1d16a78cf2
antaww <anta.pizzetta@gmail.com>
2021-09-20 09:27:54 +0200

    makerange

--- makerange.go
+++ makerange.go
@@ -1,14 +1,12 @@
 package piscine
 
 func MakeRange(min, max int) []int {
-	tableau := make([]int, max-min)
 	if min >= max {
 		return nil
 	}
-	if max >= min {
-		for i := range tableau {
-			tableau[i] += min + i
-		}
+	tableau := make([]int, max-min)
+	for i := range tableau {
+		tableau[i] += min + i
 	}
 	return tableau
 }
