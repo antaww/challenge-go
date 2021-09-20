7a67ea0b0826e39de7d9b6dd48cb714415816d13
antaww <anta.pizzetta@gmail.com>
2021-09-20 15:15:01 +0200

    issorted

--- issorted.go
+++ issorted.go
@@ -3,6 +3,9 @@ package piscine
 func IsSorted(f func(a, b int) int, a []int) bool {
 	decroissant := true
 	for i := range a {
+		if i == len(a)-1 {
+			break
+		}
 		if f(a[i], a[i+1]) > 0 { // i < i+1
 			if i == 0 {
 				decroissant = true
