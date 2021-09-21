a3dfe00e1ede79d8bd2c33cd2ee6e8ae0d530ae7
antaww <anta.pizzetta@gmail.com>
2021-09-21 11:46:33 +0200

    strrev

--- strrev.go
+++ strrev.go
@@ -2,7 +2,7 @@ package piscine
 
 func StrRev(s string) string {
 	sRune := []rune(s)
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+	for i, j := 0, len(sRune)-1; i < j; i, j = i+1, j-1 {
 		sRune[i], sRune[j] = sRune[j], sRune[i]
 	}
 	return string(s)
