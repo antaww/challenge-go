92f4193024419d4fb694d76dedee94a6896b583a
antaww <anta.pizzetta@gmail.com>
2021-09-28 14:46:30 +0200

    printprogramname

---
+++ printprogramname/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	slashindex := -1
+	for i, letter := range os.Args[0] {
+		if letter == "/" {
+			slashindex = i
+		}
+		for _, letter := range os.Args[0][slashindex+1:] {
+			z01.PrintRune(letter)
+		}
+		z01.PrintRune('\n')
+	}
+}
