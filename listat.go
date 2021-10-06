181a073a0c043f9ae0d4b6d4b1d47f4fddfa992a
antaww <anta.pizzetta@gmail.com>
2021-10-06 12:05:18 +0200

    at

--- listat.go
+++ listat.go
@@ -2,19 +2,15 @@ package piscine
 
 func ListAt(l *NodeL, pos int) *NodeL {
 	count := 0
-	count2 := 0
-	for l != nil {
-		count += 1
-		l = l.Next
+	if pos == 0 {
+		return l
 	}
-	if pos <= count {
-		for l != nil {
-			if count2 == pos {
-				return l
-			}
-			count2 += 1
-			l = l.Next
+	for count <= pos {
+		if l == nil {
+			return nil
 		}
+		l = l.Next
+		count++
 	}
-	return nil
+	return l
 }
