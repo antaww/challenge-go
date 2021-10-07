2ca34ded6d6c6dac970e0e90fc121c80d6092f45
antaww <anta.pizzetta@gmail.com>
2021-10-07 13:09:38 +0200

    rot14

--- rot14.go
+++ rot14.go
@@ -2,15 +2,15 @@ package piscine
 
 func Rot14(s string) string {
 	sRune := []rune(s)
-	lettre := true
 	for i := 0; i < len(s); i++ {
-		if verif(sRune[i]) == true && lettre {
-			if (sRune[i] >= 'a' && sRune[i] <= 'z') || (sRune[i] >= 'A' && sRune[i] <= 'Z') {
+		if verif(sRune[i]) == true {
+			if sRune[i] >= 'a' && sRune[i] <= 'z' {
 				sRune[i] -= 12
 			}
-			lettre = false
+			if sRune[i] >= 'A' && sRune[i] <= 'Z' {
+				sRune[i] -= 12
+			}
 		} else if verif(sRune[i]) == false {
-			lettre = true
 		}
 	}
 	return string(sRune)
